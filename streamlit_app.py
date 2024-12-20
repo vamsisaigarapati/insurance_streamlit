38a1917f2c42d6a6652e2f1f2395a52b80768c04
Vamsi Sai Garapati <35292779+vamsisaigarapati@users.noreply.github.com>
2024-12-20 15:28:15 -0600

    server address update

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ user_options
 
 if st.button('Predict'):
     data = json.dumps(user_options, indent=2)
-    r = requests.post('http://127.0.0.1:8000/predict', data=data)
+    r = requests.post('http://146.190.77.217:8002/predict', data=data)
     st.write(r.json())
